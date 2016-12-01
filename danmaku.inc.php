f03e2b1b5f968433e1f346768eb7ff34dcae6a9b
Qool <laigq2015@163.com>
2016-12-01 19:23:45 +0800

    发送弹幕加中转后端

--- danmaku.inc.php
+++ danmaku.inc.php
@@ -4,7 +4,7 @@ $content = file_get_contents('php://input');
 $content = json_decode($content, true);
 $danmakuCh = curl_init();
 
-$danmakuApi = "http://my.nuaa.edu.cn/office/ajax.php?class=danmaku";
+$danmakuApi = "http://localhost/office/ajax.php?class=danmaku";
 if(isset($content['token']) && ($content['token'] == 'mynuaa-video')) {
     $data['text'] = $content['text'];
     $data['username'] = $_G['username'];
@@ -12,15 +12,19 @@ if(isset($content['token']) && ($content['token'] == 'mynuaa-video')) {
     $data['type'] = isset($content['type']) ? $content['type'] : 'right';
     $data['color'] = isset($content['color']) ? $content['color'] : '#FFF';
     $data['token'] = $content['token'];
+    $data = json_encode($data);
+    $config = array(
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_URL => $danmakuApi,
+        CURLOPT_POST => 1
+        );
+    curl_setopt_array($danmakuCh,$config);
+    $content = curl_exec($danmakuCh);
+    curl_close($danmakuCh);
+
 } else if($_GET['player']){
     $lastId = (isset($_GET['lastId'])) ? intval($_GET['lastId']) : 0;
     $danmakuApi = $danmakuApi . "&lastId={$lastId}";
-    $config = array(
-      CURLOPT_URL => $danmakuApi,
-      CURLOPT_POST => false,
-    )
-    curl_setopt_array($danmakuCh, $config);
+    $return = file_get_contents($danmakuApi);
+    echo $return;
 }
-$output = curl_exec($danmakuCh);
-curl_close($danmakuCh);
-echo $output;
