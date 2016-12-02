c0dd1e023210b8fcc6bfa6f5426952f882335557
Rex Zeng <rex.fgtsky@qq.com>
2016-12-02 14:04:48 +0800

    .

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var useHotKey = !/mobile/i.test(navigator.userAgent);
+var isMobile = /mobile/i.test(navigator.userAgent);
 var player = new DPlayer({
     element: document.getElementById('player'),
-    autoplay: true,
+    autoplay: !isMobile,
     theme: '#FADFA3',
     loop: true,
-    hotkey: useHotKey,
+    hotkey: !isMobile,
     video: {
         url: 'http://211.65.102.6:8080/hls/box.m3u8'
     },
     danmaku: {
-        id: 'live:away',
+        id: '1',
         api: '/plugin.php?id=live_party:danmaku',
         token: 'mynuaa-video'
     }
